7f383e38ec1466612dabbfdf3c0e7f6b5c353692
Magnustin <126540105+Magnustin@users.noreply.github.com>
2023-02-28 23:35:58 -0300

    Add files via upload

---
+++ Prueba/java.js
@@ -0,0 +1,8 @@
+
+function myFunction() {document.getElementById("contacto").innerHTML="llamame pa 3513224568";}
+
+function mostrarTexto() {
+    var texto = "cualquiera";
+    document.getElementById("texto").innerHTML = "no tengo mas información";
+  }
+  
